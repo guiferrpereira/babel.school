e22178f483dcbf3dc09fd2782e8218cb8f7c7deb
Guilherme Pereira <guiferrpereira@gmail.com>
2015-11-04 01:26:59 +0000

    Changes colors and some texts

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -41,9 +41,9 @@ $(document).ready(
   function() { 
 
     $("html").niceScroll({
-    	cursorcolor:"#f74d65",
+    	cursorcolor:"#007ac0",
     	scrollspeed :"100",
-    	cursorborder:"1px solid #f74d65",
+    	cursorborder:"1px solid #007ac0",
     	horizrailenabled: "false",
     	cursorborderradius: "0px"
     });
